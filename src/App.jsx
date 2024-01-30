82e7b93640fe23a882db6ff8b56afd75ab6c218d
arush1103 <sharma.arush.hp@gmail.com>
2024-01-30 13:32:27 +0530

    redux screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
-import './App.css'
+import './index.css';
+import store from './lib/store';
+
+import { Provider } from 'react-redux';
+import InboxScreen from './components/InboxScreen';
 
 function App() {
 
 
   return (
-    <div className="App">
-      Bello!
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   )
 }
 
